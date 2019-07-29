1279c898c319c24e628aab89229a2eb7b920b653
anthonygduarte <52419234+anthonygduarte@users.noreply.github.com>
2019-07-29 17:26:53 +0100

    UserBusiness, Models User

--- Testes-RecemLicenciados/src/main/java/pt/aubay/testesproject/models/Models.java
+++ Testes-RecemLicenciados/src/main/java/pt/aubay/testesproject/models/Models.java
@@ -1,5 +1,13 @@
 package pt.aubay.testesproject.models;
 
 public class Models {
+	protected long id;
+	
+	public long getID() {
+		return id;
+	}
 
+	public void setID(long id) {
+		this.id = id;
+	}
 }
