4c31780123fd9d913055559edffc055f6329a610
Francisco <franciscopinto90@gmail.com>
2019-07-29 17:10:19 +0100

    Repositories and UserRepositories added

---
+++ Testes-RecemLicenciados/src/main/java/pt/aubay/testesproject/repositories/UserRepositories.java
@@ -0,0 +1,36 @@
+package pt.aubay.testesproject.repositories;
+
+import java.util.List;
+
+import javax.faces.bean.RequestScoped;
+import javax.persistence.Query;
+
+@RequestScoped
+public class UserRepositories extends Repositories<User> {
+
+	@Override
+	protected Class<User> getEntityClass() {
+		// TODO Auto-generated method stub
+		return User.class;
+	}
+	
+//Metodos que podem ser uteis - falta fazer a query correspondente 	
+	
+//	public List<User> getAll() {
+//
+//		Query query = em.createNamedQuery("User.getAll", getEntityClass());
+//		return  query.getResultList();
+//	}
+//	
+//	public long count() {
+//		Query query = em.createNamedQuery("User.count");
+//		return (long)query.getSingleResult();
+//	}
+//	
+//	public boolean productExists(long id) {	
+//		Query query = em.createNamedQuery("User.checkIfExists");
+//		query.setParameter("id", id);
+//		return (long)query.getSingleResult() == 1;
+//	}
+
+}
