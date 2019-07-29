943671f883e925266a4254b4cc9d98e9ed9845e9
anthonygduarte <52419234+anthonygduarte@users.noreply.github.com>
2019-07-29 18:24:09 +0100

    Business - add, get

--- Testes-RecemLicenciados/src/main/java/pt/aubay/testesproject/business/UserBusiness.java
+++ Testes-RecemLicenciados/src/main/java/pt/aubay/testesproject/business/UserBusiness.java
@@ -1,37 +1,52 @@
 package pt.aubay.testesproject.business;
 
+import javax.inject.Inject;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import pt.aubay.testesproject.models.User;
+import pt.aubay.testesproject.repositories.UserRepositories;
 import pt.aubay.testesproject.utils.PasswordUtils;;
 
 public class UserBusiness {
 	@Inject
-	UserRepository userRepository;
+	UserRepositories userRepository;
 	
 	public Response add(String username, String password){
 		String [] pass;
+		User user=new User();
 		Response response=checkIfUsernameValid(username);
 		if(response==Response.ok().entity("Success").build()) {
-			User user;
 			user.setUsername(username);
 			user.setPassword(passwordToHashcode(password));
-			userRespository.addEntity(user);
-		}
-		return response;
-	}
-	
-	public Response get(String username, String password){
-		Response response=Response.ok().entity("Success").build();
-		if(response.getStatus()==Response.Status.OK.getStatusCode()){
 			userRepository.addEntity(user);
 		}
 		return response;
 	}
 	
+	public Response get(String username, String password){
+		Response response=checkIfUserValid(username, password);
+		if(response!=Response.ok().entity("Success").build())
+			return response;
+		return Response.ok(userRepository.getUser(username), MediaType.APPLICATION_JSON).build();
+	}
+	
 	public Response checkIfUsernameValid(String username) {
 		if(userRepository.getUser(username)==null)
 			return Response.status(Status.NOT_FOUND).entity("Não existe na base de dados").build();
+		return Response.ok().entity("Success").build();
+	}
+	
+	public Response checkIfUserValid(String username, String password) {
+		User myUser=userRepository.getUser(username);
+		String[] hashCode=myUser.getPassword();
+		String key=hashCode[0];
+		String salt=hashCode[1];
+		Response response=checkIfUsernameValid(username);
+		if(response!=Response.ok().entity("Success").build())
+			return response;
+		if(!PasswordUtils.verifyPassword(password, key, salt))
+			return Response.status(Status.FORBIDDEN).entity("Palavra-passe inválida").build();
 		return Response.ok().entity("Success").build();
 	}
 	
