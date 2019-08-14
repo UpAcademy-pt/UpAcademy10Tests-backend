aaafa3de0c3128375afcc5c45c4d34c109855251
anthonygduarte <52419234+anthonygduarte@users.noreply.github.com>
2019-08-14 18:38:54 +0100

    Important changes - imports

--- Testes-RecemLicenciados/src/main/resources/META-INF/data/import.sql
+++ Testes-RecemLicenciados/src/main/resources/META-INF/data/import.sql
@@ -1,1 +1,4 @@
-INSERT INTO Category ([category]) VALUES ("java");
+INSERT INTO Category(category) VALUES ("JAVA");
+INSERT INTO Category(category) VALUES ("SQL");
+INSERT INTO Category(category) VALUES ("JAVASCRIPT");
+INSERT INTO RegisteredUser(username, email, accessType, available, salt, hashcode, lastLogin) VALUES ("admin", "admin@admin.pt", "Admin", true, "WcRncmJ2ox8qdydamEqK4VVy7yDObEEdOy+H5QX6RSsNb6KhQxwJiDNWa3IzeBUCmAw=", "2oMz4wy1", "2019-08-14 17:32:58");
